24932fc2fe7f8e027028212ef0765a8c03f0dbc6
abdulso.dev <frontenddeveloperabdul@gmail.com>
2023-10-06 15:07:53 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors : {
+        'minsk': {
+          '50': '#faf7fd',
+          '100': '#f2ecfb',
+          '200': '#e7dcf8',
+          '300': '#d5c0f2',
+          '400': '#bb98e8',
+          '500': '#a070dc',
+          '600': '#8b51cc',
+          '700': '#753eb2',
+          '800': '#633792',
+          '900': '#522e76',
+          '950': '#351655',
+      },
+      
+      }
+
+      
+    },
+  },
+  plugins: [],
+}
+
